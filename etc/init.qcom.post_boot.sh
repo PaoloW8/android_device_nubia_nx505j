d89800dd067594e01ae7412c5795d75340db60c9
PaoloW8 <paolom970@gmail.com>
2015-08-12 18:35:54 +0200

    update to release V6

--- etc/init.qcom.post_boot.sh
+++ etc/init.qcom.post_boot.sh
@@ -282,20 +282,13 @@ case "$target" in
                 do
                     echo "cpubw_hwmon" > $devfreq_gov
                 done
-                echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-                echo "interactive" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-                echo "interactive" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-                echo "interactive" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+                echo "intelliactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+                echo "intelliactive" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+                echo "intelliactive" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+                echo "intelliactive" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
                 echo "20000 1400000:40000 1700000:20000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-                echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
-                echo 1497600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
-                echo 1497600 > /sys/devices/system/cpu/cpufreq/interactive/input_boost_freq
-                echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
-                echo "85 1500000:90 1800000:70" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
-                echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
                 echo 20 > /sys/module/cpu_boost/parameters/boost_ms
                 echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold
-                echo 100000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
                 echo 1497600 > /sys/module/cpu_boost/parameters/input_boost_freq
                 echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
                 setprop ro.qualcomm.perf.cores_online 1
@@ -522,11 +515,8 @@ case "$target" in
     ;;
     "msm8974")
         start mpdecision
-	sleep 1
-	stop mpdecision
-	sleep 1
-	start mpdecision
         echo 4096 > /sys/block/mmcblk0/bdi/read_ahead_kb
+	sysctl -w net.ipv4.tcp_congestion_control=westwood
     ;;
     "apq8084")
         rm /data/system/default_values
--- etc/powerprofile.sh
+++ etc/powerprofile.sh
@@ -31,15 +31,10 @@ dev_governor=`ls /sys/class/devfreq/qcom,cpubw*/governor`
                  echo 2457600                              > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
                  echo 2457600                              > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
                  echo 2                                    > /sys/devices/system/cpu/sched_mc_power_savings
-                 echo interactive                          > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-                 echo interactive                          > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-                 echo interactive                          > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-                 echo interactive                          > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-                 echo "20000 1400000:40000 1700000:20000"  > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-                 echo 90                                   > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
-                 echo 1497600                              > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
-                 echo "85 1500000:99"                      > /sys/devices/system/cpu/cpufreq/interactive/target_loads
-                 echo 40000                                > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+                 echo intelliactive                        > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+                 echo intelliactive                        > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+                 echo intelliactive                        > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+                 echo intelliactive                        > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
                  echo 20                                   > /sys/module/cpu_boost/parameters/boost_ms
                  echo 1728000                              > /sys/module/cpu_boost/parameters/sync_threshold
                  echo 1497600                              > /sys/module/cpu_boost/parameters/input_boost_freq
